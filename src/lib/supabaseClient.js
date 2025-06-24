f872a5d61f374367847a05106fee70791ca7304f
kalana kavinda <157974554+Kalana8@users.noreply.github.com>
2025-06-24 19:54:37 +0530

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://yktgocjkdyuehklxgfco.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrdGdvY2prZHl1ZWhrbHhnZmNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkzOTE1MzQsImV4cCI6MjA2NDk2NzUzNH0.aIRQLIK3NuKcy8e6d9BkRhYhurfGYrpS8o7s22W-4aw';
+VITE_SUPABASE_URL="your_supabase_url"
+VITE_SUPABASE_ANON_KEY="your_supabase_anon_key"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
